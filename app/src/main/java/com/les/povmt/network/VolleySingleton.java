aeb0aa67da68e9c9d15d50421294b530c1938f7a
samueltcsantos <thiagsamuel@gmail.com>
2016-11-25 17:02:07 -0300

    fix volley error (removed cache parameter)

--- app/src/main/java/com/les/povmt/network/VolleySingleton.java
+++ app/src/main/java/com/les/povmt/network/VolleySingleton.java
@@ -51,10 +51,7 @@ public class VolleySingleton   {
 
     private RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-           // mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext(), 10 * 1024 * 1024);
-            // this for caching
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
         }
         return mRequestQueue;
     }
