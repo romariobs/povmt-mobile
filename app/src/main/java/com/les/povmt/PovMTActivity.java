ebb7d7409c07477e76c1596a52f42b3ea88beb66
samueltcsantos <thiagsamuel@gmail.com>
2016-11-25 18:56:47 -0300

    Added new icones to side menu

--- app/src/main/java/com/les/povmt/PovMTActivity.java
+++ app/src/main/java/com/les/povmt/PovMTActivity.java
@@ -87,11 +87,11 @@ public class PovMTActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_activities) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_weekly_report) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_history) {
 
         } else if (id == R.id.nav_manage) {
 
