0a095e9b792a61643d4525ac8e4528f8998c67e9
Caio Juvino <caio.queiroz@ccc.ufcg.edu.br>
2016-11-29 20:03:22 -0300

    some tab swapping adjustments

--- app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
+++ app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
@@ -17,6 +17,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.les.povmt.R;
 import com.les.povmt.network.VolleySingleton;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -30,6 +31,9 @@ public class FirstTabFragment extends Fragment{
     private static String URL = "http://povmt.herokuapp.com/history?startDate=%s&endDate=%s&creator=1";
     TextView mTextView;
     StringRequest stringRequest;
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    String weekSunday = "2016-11-27";
+    String today = df.format(new Date());
 
     public FirstTabFragment() {
         // Required empty public constructor
@@ -38,10 +42,6 @@ public class FirstTabFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        String weekSunday = "2016-11-27";
-        String today = df.format(new Date());
         URL = String.format(URL,weekSunday,today);
 
         System.out.println(URL);
@@ -57,6 +57,15 @@ public class FirstTabFragment extends Fragment{
         // (2) create a date "formatter" (the date format we want)
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss");*/
 
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        mTextView = (TextView) view.findViewById(R.id.textview_frag1);
         final ProgressDialog loading = new ProgressDialog(getContext(), R.style.AppThemeDarkDialog);
 
         loading.setMessage("Carregando...");
@@ -64,43 +73,41 @@ public class FirstTabFragment extends Fragment{
 
         // Request a string response from the provided URL.
         stringRequest = new StringRequest(Request.Method.GET, "http://povmt.herokuapp.com/history?startDate=2016-11-01&endDate=2016-11-07&creator=1", new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+            @Override
+            public void onResponse(String response) {
 
-                        // Display the first 500 characters of the response string.
-                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                // Display the first 500 characters of the response string.
+                //mTextView.setText("Response is: "+ response.substring(0,500));
 
-                        JSONObject json;
+                JSONObject json;
 
-                        try {
-                            json = new JSONObject(response);
-                            int status = 0;
+                try {
+                    json = new JSONObject(response);
+                    int status = 0;
 
-                            if (json.has("status")){
-                                status = json.getInt("status");
-                            }
-
-                            if (status == HTTP_OK) {
-                                loading.cancel();
-                            } else {
-                                loading.cancel();
-                                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                                builder.setMessage("Não entendi")
-                                        .setNegativeButton("ok", null)
-                                        .create()
-                                        .show();
-                            }
-
-                            mTextView.setText("Response is: "+ json);
-                            mTextView.setText("Response is: "+ json.getJSONObject("history").optJSONArray("its"));
-                        } catch (JSONException e){
-                            System.out.println(response);
-                            Log.e("JSON","FAILED");
-                        }
-
-
+                    if (json.has("status")){
+                        status = json.getInt("status");
                     }
-                }, new Response.ErrorListener() {
+                    loading.cancel();
+
+                    if (status != HTTP_OK) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setMessage(status).setNegativeButton("ok", null)
+                                .create().show();
+                    }
+                    JSONArray jIts = json.getJSONObject("history").optJSONArray("its");
+
+                    String text = "";
+                    for (int its = 0; its < jIts.length(); its++) {
+                        text = text + jIts.get(its).toString() + "\n\n";
+                    }
+                    mTextView.setText(text);
+                } catch (JSONException e){
+                    System.out.println(response);
+                    Log.e("JSON","FAILED");
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 loading.cancel();
@@ -110,14 +117,7 @@ public class FirstTabFragment extends Fragment{
         });
 
         VolleySingleton.getInstance(getContext()).addToRequestQueue(stringRequest);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_one, container, false);
-        mTextView = (TextView) view.findViewById(R.id.textview_frag1);
+        //mTextView.setText("Test");
         return view;
     }
 }
