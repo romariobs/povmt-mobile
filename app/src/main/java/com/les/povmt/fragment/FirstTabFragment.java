57e1c427b6d3d8c23524a9b46bfce793d56b3c65
Caio Juvino <caio.queiroz@ccc.ufcg.edu.br>
2016-11-29 22:04:17 -0300

    parsing its from history json

--- app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
+++ app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
@@ -15,7 +15,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.les.povmt.R;
+import com.les.povmt.models.Activity;
+import com.les.povmt.models.InvestedTime;
 import com.les.povmt.network.VolleySingleton;
+import com.les.povmt.parser.InvestedTimeParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,11 +26,12 @@ import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
 public class FirstTabFragment extends Fragment{
-    private static String hostURL = "http://povmt.herokuapp.com/history?startDate=%s&endDate=%s&creator=1";
+    private static String hostURL = "http://povmt.herokuapp.com/history?startDate=%s&endDate=%s&creator=583e15b27eccdc0400798c22";
     String sampleURL = "http://povmt.herokuapp.com/history?startDate=2016-11-01&endDate=2016-11-07&creator=1";
     TextView mTextView;
     StringRequest stringRequest;
@@ -35,29 +39,12 @@ public class FirstTabFragment extends Fragment{
     String weekSunday = "2016-11-27";
     String today = df.format(new Date());
 
-    public FirstTabFragment() {
-        // Required empty public constructor
-    }
+    public FirstTabFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         hostURL = String.format(hostURL,weekSunday,today);
-
-        System.out.println(hostURL);
-        System.out.println(weekSunday);
-        System.out.println(today);
-
-        /*
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        Calendar cal = Calendar.getInstance();
-
-        Date today = Calendar.getInstance().getTime();
-
-        // (2) create a date "formatter" (the date format we want)
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss");*/
-
-
     }
 
     @Override
@@ -95,12 +82,20 @@ public class FirstTabFragment extends Fragment{
                         builder.setMessage(status).setNegativeButton("ok", null)
                                 .create().show();
                     }
+
                     JSONArray jIts = json.getJSONObject("history").optJSONArray("its");
 
+                    //PARSING ITs FROM HISTORY
+                    List<InvestedTime> itsList = (new InvestedTimeParser()).parse(json.getJSONObject("history").toString());
                     String text = "";
-                    for (int its = 0; its < jIts.length(); its++) {
-                        text = text + jIts.get(its).toString() + "\n\n";
+
+                    for (int it = 0; it < jIts.length(); it++) {
+                        InvestedTime invTime = itsList.get(it);
+                        text = text + "Id do TI: " + invTime.getId()  + "\nId da Atividade: " + invTime.getActivityId() +
+                                "\nDuração: " + invTime.getDuration() + "\nData de Criação: " + invTime.getDate() + "\n\n";
+                        System.out.println(itsList.get(it));
                     }
+
                     mTextView.setText(text);
                 } catch (JSONException e){
                     System.out.println(response);
---
+++ app/src/main/java/com/les/povmt/parser/HistoryParser.java
@@ -0,0 +1,20 @@
+package com.les.povmt.parser;
+
+import com.les.povmt.models.Activity;
+
+import java.util.List;
+
+/**
+ * Created by caio on 29/11/2016.
+ */
+
+public class HistoryParser {
+    List<Activity> activities;
+    private final String TAG = this.getClass().getSimpleName();
+
+    public  HistoryParser(){
+
+    }
+
+
+}
