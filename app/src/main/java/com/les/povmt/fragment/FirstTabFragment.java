4d5b23a4121f7fb691633ee052fd94c8e6bd3afd
Caio Juvino <caio.queiroz@ccc.ufcg.edu.br>
2016-12-12 23:01:39 -0300

    history request fix

--- app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
+++ app/src/main/java/com/les/povmt/fragment/FirstTabFragment.java
@@ -19,9 +19,12 @@ import com.les.povmt.R;
 import com.les.povmt.models.Activity;
 import com.les.povmt.models.InvestedTime;
 import com.les.povmt.models.User;
+import com.les.povmt.network.RestClient;
 import com.les.povmt.network.VolleySingleton;
 import com.les.povmt.parser.ActivityParser;
 import com.les.povmt.parser.InvestedTimeParser;
+import com.les.povmt.util.Constants;
+import com.les.povmt.util.Messages;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,6 +34,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
@@ -39,7 +44,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 public class FirstTabFragment extends Fragment{
     private Date startDay, endDay;
     private DateFormat dfServer = new SimpleDateFormat("yyyy-MM-dd");
-    private String hostURL = "http://povmt.herokuapp.com/history?startDate=";
+    private String hostURL = "http://povmt.herokuapp.com/history";
     private StringRequest stringRequest;
     private List<String> dataSource;
 
@@ -55,7 +60,7 @@ public class FirstTabFragment extends Fragment{
         cal.add(Calendar.WEEK_OF_YEAR, 1);
         endDay = cal.getTime();
 
-        hostURL += dfServer.format(startDay) + "&endDate=";
+        hostURL += "?startDate=" + dfServer.format(startDay) + "&endDate=";
         hostURL += dfServer.format(endDay) + "&creator=";
         hostURL += User.getCurrentUser().getId();
     }
@@ -71,6 +76,7 @@ public class FirstTabFragment extends Fragment{
         dataSource = new ArrayList<>();
         final ListView lView = (ListView)view.findViewById(R.id.list1);
 
+        /*
         stringRequest = new StringRequest(Request.Method.GET, hostURL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -142,6 +148,88 @@ public class FirstTabFragment extends Fragment{
             }
         });
         VolleySingleton.getInstance(getContext()).addToRequestQueue(stringRequest);
+        */
+
+        String finalRequest = hostURL;
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.d("ON RESP", response);
+
+                JSONObject json;
+
+                try {
+                    json = new JSONObject(response);
+                    int status = 0;
+
+                    if (json.has("status")){
+                        status = json.getInt("status");
+                    }
+                    loading.cancel();
+
+                    if (status != HTTP_OK) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setMessage(status).setNegativeButton("ok", null)
+                                .create().show();
+                    }
+                    String text = "";
+
+                    JSONObject group = json.getJSONObject("history").getJSONArray("groupedHistory")
+                            .optJSONObject(0);
+
+                    System.out.println(response);
+
+                    if (group!= null) {
+                        JSONArray arrayIts = group.getJSONArray("its");
+
+                        //PARSING ITs FROM HISTORY
+                        List<Activity> activities = (new ActivityParser()).parseFromHistory(json.getJSONObject("history").toString());
+                        List<InvestedTime> itsList = (new InvestedTimeParser()).parse(group.toString());
+
+                        for(int j = 1; j < json.getJSONObject("history").getJSONArray("groupedHistory").length();j++){
+                            group = json.getJSONObject("history").getJSONArray("groupedHistory").optJSONObject(j);
+                            List<InvestedTime> varList = (new InvestedTimeParser()).parse(group.toString());
+                            itsList.addAll(varList);
+                        }
+
+                        for (int it = 0; it < itsList.size(); it++) {
+                            InvestedTime invTime = itsList.get(it);
+                            String actName = "";
+
+                            for (Activity act : activities) {
+                                if(act.getId().equals(invTime.getActivityId()))
+                                    actName = act.getTitle();
+                            }
+
+                            Calendar cal = invTime.getOriginalDate();
+                            text = "Atividade: " + actName + "\nTempo Investido: " + invTime.getDuration() + " minutos"
+                                    + "\nEm " + invTime.getDate();
+                            dataSource.add(text);
+                        }
+                    }
+                    ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),R.layout.rowlayout,R.id.txtitem, dataSource);
+                    lView.setAdapter(adapter);
+                } catch (JSONException e){
+                    Log.e("JSON","FAILED");
+                }
+            };
+
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading.cancel();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Volley Error");
+                builder.setMessage(error.toString()).setNegativeButton("OK", null)
+                        .create().show();
+            }
+        };
+
+        RestClient.get(getContext(), finalRequest, responseListener, errorListener);
+
         return view;
     }
 }
